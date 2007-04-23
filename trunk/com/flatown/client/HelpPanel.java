aae82330c614bacbbb7431872a1ff5c06e869ba3
jflatow <jflatow@a855c384-3f2e-0410-86b2-8fc0afea6a7a>
2007-04-23 03:41:03 +0000

    Fixed directory structure with com/

---
+++ trunk/com/flatown/client/HelpPanel.java
@@ -0,0 +1,12 @@
+package com.flatown.client;
+
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class HelpPanel extends VerticalPanel {
+  
+  public static final HelpPanel Singleton = new HelpPanel();
+  
+  private HelpPanel() {
+    
+  }
+}
