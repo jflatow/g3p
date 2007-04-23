1496b4d6987684de55ccbbe0ed13db3ce7f3fc2a
jflatow <jflatow@a855c384-3f2e-0410-86b2-8fc0afea6a7a>
2007-04-23 03:41:03 +0000

    Fixed directory structure with com/

---
+++ com/flatown/client/FavoritesPanel.java
@@ -0,0 +1,12 @@
+package com.flatown.client;
+
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class FavoritesPanel extends VerticalPanel {
+  
+  public static final FavoritesPanel Singleton = new FavoritesPanel();
+  
+  private FavoritesPanel() {
+    
+  }
+}
---
+++ com/flatown/client/SearchPanel.java
@@ -0,0 +1,12 @@
+package com.flatown.client;
+
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class SearchPanel extends VerticalPanel {
+  
+  public static final SearchPanel Singleton = new SearchPanel();
+  
+  private SearchPanel() {
+    
+  }
+}
